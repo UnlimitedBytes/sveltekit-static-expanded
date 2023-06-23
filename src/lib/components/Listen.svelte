e4ee2bd03d37c4a40c9698afd1d1c08b8ffbf34b
UnlimitedBytes <admin@unlimitedbytes.ovh>
2023-06-23 02:00:12 +0200

    feat: add Listen component

---
+++ src/lib/components/Listen.svelte
@@ -0,0 +1,22 @@
+<script lang="ts">
+	export let src: string;
+
+	let active = false;
+</script>
+
+<div class="listen">
+	{#if active}
+		<audio controls autoplay {src} />
+	{:else}
+		<button on:click={() => (active = true)}>Listen to article</button>
+	{/if}
+</div>
+
+<style>
+	.listen {
+		width: 100%;
+		display: flex;
+		justify-content: end;
+		margin-bottom: 1rem;
+	}
+</style>
